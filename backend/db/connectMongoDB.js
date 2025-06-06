348ca550b5cb5c39be2bf28ce8f1ddcde2b967b8
Madhujya <your.email@example.com>
2025-06-06 23:09:49 +0530

    completed Mongodb Schema degine

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,18 @@
+
+import mongoose,{connect} from "mongoose";
+import {DB_NAME} from "../constant.js"
+
+const connectMongoDB=async ()=>{
+    try{
+        const connectioninstance=await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+       console.log(`\n database connected !! DB_host:${connectioninstance.connection.host}`);
+
+    }catch(error)
+
+    {
+        console.log("MONGODB connection error:",error);
+        process.exit(1)
+    }
+}
+
+export default connectMongoDB
