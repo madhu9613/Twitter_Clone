cc4ce0124c58e385dec21a03095d9fe8384c64f0
madhu9613 <rajkhowamadhujya0@gmail.com>
2025-07-02 01:06:04 +0530

    fixing theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
@@ -9,15 +9,8 @@ export default {
 	plugins: [daisyui],
 
 	daisyui: {
-		themes: [
-			"light",
-			{
-				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
-				},
-			},
-		],
-	},
+    themes: ["light", "dark", "cupcake","black"]
+  },
+
+
 };
